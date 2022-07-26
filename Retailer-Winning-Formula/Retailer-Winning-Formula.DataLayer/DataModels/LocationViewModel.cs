ea72611020786d78d4b62200227ea1209d170ace
kirilluser22 <kyryloupworkuser22@gmail.com>
2022-07-26 11:10:01 +0300

    added model

---
+++ Retailer-Winning-Formula/Retailer-Winning-Formula.DataLayer/DataModels/LocationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Retailer_Winning_Formula.DataLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TimeZone = Retailer_Winning_Formula.DataLayer.Entities.TimeZone;
+
+namespace Retailer_Winning_Formula.DataLayer.DataModels
+{
+    public class LocationViewModel
+    {
+        public int RowNo { get; set; }
+        public List<TimeZone> TimeZones { get; set; }
+        public List<AnnualSalesVolume> AnnualSalesVolumes { get; set; }
+    }
+}
