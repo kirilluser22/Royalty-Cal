dc2f5ac6a2c5dd85d8373ca7916d790fa7f3faac
kirilluser22 <kyryloupworkuser22@gmail.com>
2022-07-29 08:00:49 +0300

    added EF

---
+++ Retailer-Winning-Formula/Retailer-Winning-Formula.DataLayer/Entities/PartialClasses.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Retailer_Winning_Formula.DataLayer.JsonClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Retailer_Winning_Formula.DataLayer.Entities
+{
+    public partial class UserReports
+    {
+        [NotMapped]
+        public DefaultFactor _DefaultFactors
+        {
+            get
+            {
+                return JsonConvert.DeserializeObject<DefaultFactor>(string.IsNullOrEmpty(DefaultFactors) ? "{}" : DefaultFactors);
+            }
+            set
+            {
+                DefaultFactors = JsonConvert.SerializeObject(string.IsNullOrEmpty(DefaultFactors) ? "{}" : DefaultFactors);
+            }
+        }
+    }
+}
