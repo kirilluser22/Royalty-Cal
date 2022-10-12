2fc946a0f1a2462ed318010353912af54f6f1d4d
kirilluser22 <kyryloupworkuser22@gmail.com>
2022-10-12 12:24:36 +0300

    entityconfiguration

---
+++ Retailer-Winning-Formula/Retailer-Winning-Formula.DataLayer/EntityConfigurations/SettingsConfiguration.cs
@@ -0,0 +1,18 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Metadata.Builders;
+//using Retailer_Winning_Formula.Entities;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace Retailer_Winning_Formula.DataLayer.EntityConfigurations
+//{
+//    public class SettingsConfiguration : IEntityTypeConfiguration<Settings>
+//    {
+//        public void Configure(EntityTypeBuilder<Settings> builder)
+//        {
+//            builder.HasKey(s => s.Key);
+//        }
+
+//    }
+//}
---
+++ Retailer-Winning-Formula/Retailer-Winning-Formula.DataLayer/EntityConfigurations/UserReportsConfiguration.cs
@@ -0,0 +1,17 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Metadata.Builders;
+//using Newtonsoft.Json;
+//using Retailer_Winning_Formula.Entities;
+
+//namespace Retailer_Winning_Formula.DataLayer.EntityConfigurations
+//{
+//    public class UserReportsConfiguration : IEntityTypeConfiguration<UserReports>
+//    {
+//        public void Configure(EntityTypeBuilder<UserReports> builder)
+//        {
+//            builder.Property(e => e.DefaultFactors).HasConversion(
+//            v => JsonConvert.SerializeObject(v, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }),
+//            v => JsonConvert.DeserializeObject<DefaultFactors>(v, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore }));
+//        }
+//    }
+//}
